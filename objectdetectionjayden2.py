b2e3508933989a04ea96dcec0deb2d4ab050eefa
Jayden Lee <102839734+goveotime@users.noreply.github.com>
2024-08-10 11:25:54 +0900

    Update objectdetectionjayden2.py

--- objectdetectionjayden2.py
+++ objectdetectionjayden2.py
@@ -38,8 +38,8 @@ with HiddenPrints():
     DATA_ROOT = '/content/data'
     os.makedirs(DATA_ROOT, exist_ok=True)
 
-    !pip -q install streamlit
-    !pip -q install pyngrok
+    pip -q install streamlit
+    pip -q install pyngrok
 
     from pyngrok import ngrok
     import streamlit
@@ -47,22 +47,22 @@ with HiddenPrints():
     # image_url = 'https://drive.google.com/uc?id=12ZpZ5H0kJIkWk6y4ktGfqR5OTKofL7qw'
     # image_path = os.path.join(DATA_ROOT, 'image.jpg')
     # gdown.download(image_url, image_path, True)
-    !wget -O /content/data/image.jpg "https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20%20Object%20Detection%20(Autonomous%20Vehicles)/image.jpg"
+    wget -O /content/data/image.jpg "https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20%20Object%20Detection%20(Autonomous%20Vehicles)/image.jpg"
 
     # image2_url = 'https://drive.google.com/uc?id=1_WpFbGEuS2r19UeP6wekbcF0kb-0nH18'
     # image2_path = os.path.join(DATA_ROOT, 'image2.jpg')
     # gdown.download(image2_url, image2_path, True)
-    !wget -O /content/data/image2.jpg "https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20%20Object%20Detection%20(Autonomous%20Vehicles)/image2.jpg"
+    wget -O /content/data/image2.jpg "https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20%20Object%20Detection%20(Autonomous%20Vehicles)/image2.jpg"
 
     # video_url = 'https://drive.google.com/uc?id=1xFGjpzhZVYtNor9hJevvxysGESZJIMDz'
     # video_path = os.path.join(DATA_ROOT, 'video1.mp4')
     # gdown.download(video_url, video_path, True)
-    !wget -O /content/data/video1.mp4 "https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20%20Object%20Detection%20(Autonomous%20Vehicles)/6.mp4"
+    wget -O /content/data/video1.mp4 "https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20%20Object%20Detection%20(Autonomous%20Vehicles)/6.mp4"
 
     # model_url = 'https://drive.google.com/uc?id=19XKJWMKDfDlag2MR8ofjwvxhtr9BxqqN'
     # model_path = os.path.join(DATA_ROOT, 'yolo_weights.h5')
     # gdown.download(model_url, model_path, True)
-    !wget -O /content/data/yolo_weights.h5 "https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20%20Object%20Detection%20(Autonomous%20Vehicles)/yolo.h5"
+    wget -O /content/data/yolo_weights.h5 "https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20%20Object%20Detection%20(Autonomous%20Vehicles)/yolo.h5"
 
 
 
@@ -334,7 +334,7 @@ def launch_website():
     print("Click this link to try your web app:")
     print(tunnel.public_url)
 
-    !streamlit run --server.port 80 app.py >/dev/null # Connect to the URL through Port 80 (>/dev/null hides outputs)
+    streamlit run --server.port 80 app.py >/dev/null # Connect to the URL through Port 80 (>/dev/null hides outputs)
 
   except KeyboardInterrupt:
     ngrok.kill()
