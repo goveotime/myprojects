b5eccb17cb3244a049a53dec6c4b1886fcd6d005
Jayden Lee <102839734+goveotime@users.noreply.github.com>
2024-08-10 11:37:13 +0900

    Update objectdetectionjayden2.py

--- objectdetectionjayden2.py
+++ objectdetectionjayden2.py
@@ -95,6 +95,13 @@ with HiddenPrints():
     # gdown.download(model_url, model_path, True)
     wget -O /content/data/yolo_weights.h5 "https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20%20Object%20Detection%20(Autonomous%20Vehicles)/yolo.h5"
 
+    url = "https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20%20Object%20Detection%20(Autonomous%20Vehicles)/yolo.h5"
+    response = requests.get(url)
+
+    # Save the file locally
+    with open("/content/data/yolo_weights.h5", "wb") as file:
+        file.write(response.content)
+
 
 
 labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", \
